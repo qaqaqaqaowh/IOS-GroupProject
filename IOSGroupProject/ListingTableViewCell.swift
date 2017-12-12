3deb0bfb089134affadd6f3b6aed335a8ad2d7bf
qaqaqaqaowh <nicholasowh@hotmail.com>
2017-12-12 14:23:59 +0800

    implemented untested video player

---
+++ IOSGroupProject/ListingTableViewCell.swift
@@ -0,0 +1,56 @@
+//
+//  ListingTableViewCell.swift
+//  IOSGroupProject
+//
+//  Created by NEXTAcademy on 12/12/17.
+//  Copyright © 2017 asd. All rights reserved.
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+
+protocol ShowDetailDelegate {
+    func showDetail(withListing: Listing)
+}
+
+class ListingTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var videoView: AVPlayerViewController!
+    
+    @IBOutlet weak var viewCount: UILabel!
+    
+    var listing: Listing = Listing(withURLString: "", withViewCount: 0)
+    
+    var player = AVPlayer()
+    
+    var delegate: ShowDetailDelegate?
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        viewCount.text = String(listing.viewCount)
+        videoView.player = player
+        let tap = UITapGestureRecognizer(target: self, action: #selector(videoTapped))
+        videoView.view.isUserInteractionEnabled = true
+        videoView.view.addGestureRecognizer(tap)
+        // Initialization code
+    }
+    
+    @objc func videoTapped() {
+        guard let url = URL(string: listing.videoURL)
+            else {return}
+        player = AVPlayer(url: url)
+        player.play()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    @IBAction func showDetailButton(_ sender:UIButton) {
+        delegate?.showDetail(withListing: listing)
+    }
+
+}
